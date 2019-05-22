95bd7bca74aa7ade9d4227076adafa552e85187b
Greg Hamilton <greghamilton@cmail.carleton.ca>
2019-05-22 18:48:17 -0400

    add ability to send data to arduino via serial

---
+++ SendToArduinoTest/SendToArduinoTest/SendToArduinoTest.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::IO::Ports;
+int main(array<System::String ^> ^args)
+{
+	SerialPort port("COM3", 9600);
+	port.Open();
+	while (true) {
+		int input = Convert::ToInt32(Console::ReadLine());
+		if (input == 1) {
+			port.Write("A");
+		}
+		else {
+			port.Write("B");
+		}
+	}
+
+    return 0;
+}
