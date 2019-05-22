5e424c718fb9752aa098d661b5e7235cdaf9218d
Greg Hamilton <greghamilton@cmail.carleton.ca>
2019-05-22 19:04:44 -0400

    arduino code for changing light based on serial

---
+++ light_from_serial_monitor/light_from_serial_monitor.ino
@@ -0,0 +1,16 @@
+int led = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT); 
+}
+
+void loop() {
+  if(Serial.available()){
+    int data = Serial.read();
+    if(data == 'A')
+      digitalWrite(led,HIGH);
+    else
+      digitalWrite(led,LOW);
+  }
+  delay(500);
+} 
